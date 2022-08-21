f6244160a448244188db4c2e6eb158257966e49f
DavidHearl <david.hearl@outlook.com>
2022-08-21 15:40:11 +0100

    added user registration

--- blog/views.py
+++ blog/views.py
@@ -6,11 +6,11 @@ from django.contrib.auth.decorators import login_required
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.urls import is_valid_path
-from django.views.generic import ListView
+# from django.views.generic import ListView
 from django.core.mail import send_mail
 
-from .forms import EmailPostForm, CommentForm, LoginForm
-from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm, LoginForm, UserRegistrationForm
+from .models import Post, Comment #,Profile
 
 
 def post_list(request):
@@ -101,6 +101,20 @@ def user_login(request):
         form = LoginForm()
         
     return render(request, 'account/login.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            Profile.objects.create(user=new_user)
+            return render(request,'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
 
 
 @login_required
